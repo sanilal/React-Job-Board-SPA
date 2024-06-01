290e7ed96561466afafeecb7b814afa98b37a587
Sanilal P Sathyan <sanilal@gmail.com>
2024-06-01 11:08:29 +0400

    api added

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3002
+    port: 3002,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
