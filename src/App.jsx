ba590f98177848eeae53ff90ba963d2647db368a
Sanilal P Sathyan <sanilal@gmail.com>
2024-06-06 09:34:06 +0400

    job page ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
-import JobPage from './pages/JobPage'
+import JobPage, {jobLoader} from './pages/JobPage'
 import NotFound from './pages/NotFound'
 
 
@@ -11,7 +11,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
     <Route path='*' element={<NotFound />} />
   </Route>
 
