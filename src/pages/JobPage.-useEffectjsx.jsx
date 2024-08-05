bc31d25302241db22889b930f4440e22ea26c538
Sanilal P Sathyan <sanilal@gmail.com>
2024-08-05 09:12:17 +0400

    add job

--- src/pages/JobPage.-useEffectjsx.jsx
+++ src/pages/JobPage.-useEffectjsx.jsx
@@ -1,27 +1,44 @@
-import { useParams, useLoaderData } from "react-router-dom"
-import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
+ import { useState, useEffect } from "react"
+//import { useParams, useLoaderData } from "react-router-dom"
+import { useParams } from "react-router-dom"
+ import Spinner from "../components/Spinner";
 
 const JobPage = () => {
     const { id } = useParams();
-    const job = useLoaderData();
+    // const job = useLoaderData();
+    const [job, setJob] = useState(null)
+    const [loading, setLoading] = useState(true)
 
 
+    useEffect(() => {
+            const fetchjob = async() => {
+                try {
+                  const res = await fetch(`/api/jobs/${id}`)
+                  const data = await res.json();
+                  console.log(data);
+                  setJob(data);
+                } catch (error) {
+                  console.log('Error fetching data', error)
+                } finally {
+                  setLoading(false)
+                }
+                
+              }
+              fetchjob();
+    }, [])
 
-    
-
-  return (
+  return loading ? <Spinner /> : (
   
         <>
+        {/* <h1>{job.title}</h1> */}
           <section>
       <div className="container m-auto py-6 px-6">
-        <Link
-          to="/jobs"
+        <a
+          href="/jobs.html"
           className="text-indigo-500 hover:text-indigo-600 flex items-center"
         >
-          <FaArrowLeft className="mr-2"/> Back to Job Listings
-        </Link>
+          <i className="fas fa-arrow-left mr-2"></i> Back to Job Listings
+        </a>
       </div>
     </section>
 
@@ -32,15 +49,17 @@ const JobPage = () => {
             <div
               className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
             >
-              <div className="text-gray-500 mb-4">{job.type}</div>
+              <div className="text-gray-500 mb-4">Full-Time</div>
               <h1 className="text-3xl font-bold mb-4">
-                {job.title}
+                Senior React Developer
               </h1>
               <div
                 className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
-                <FaMapMarker className="text-lg text-orange-700 mr-2" />
-                <p className="text-orange-700">{job.location}</p>
+                <i
+                  className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
+                ></i>
+                <p className="text-orange-700">Boston, MA</p>
               </div>
             </div>
 
@@ -50,12 +69,12 @@ const JobPage = () => {
               </h3>
 
               <p className="mb-4">
-              {job.description}
+               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
               </p>
 
               <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
 
-              <p className="mb-4">{job.salary}</p>
+              <p className="mb-4">$70k - $80K / Year</p>
             </div>
           </main>
 
@@ -65,10 +84,10 @@ const JobPage = () => {
             <div className="bg-white p-6 rounded-lg shadow-md">
               <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-              <h2 className="text-2xl">{job.company}</h2>
+              <h2 className="text-2xl">NewTek Solutions</h2>
 
               <p className="my-2">
-              {job.company.description}
+                NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.
               </p>
 
               <hr className="my-4" />
@@ -76,19 +95,22 @@ const JobPage = () => {
               <h3 className="text-xl">Contact Email:</h3>
 
               <p className="my-2 bg-indigo-100 p-2 font-bold">
-              {job.company.contactEmail}
+                contact@newteksolutions.com
               </p>
 
               <h3 className="text-xl">Contact Phone:</h3>
 
-              <p className="my-2 bg-indigo-100 p-2 font-bold"> {job.company.contactPhone}</p>
+              <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
             </div>
+
+          
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-              <Link
-                href={`/jobs/edit/${job.id}`}
+              <a
+                href="/add-job.html"
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</Link>
+                >Edit Job</a
+              >
               <button
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
