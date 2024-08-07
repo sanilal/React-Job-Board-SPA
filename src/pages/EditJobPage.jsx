c60749fd44e7f5effca61733b8e035a2ee141822
Sanilal P Sathyan <sanilal@gmail.com>
2024-08-07 08:34:00 +0400

    button text changed

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -216,7 +216,7 @@ const EditJobPage = ( {updateJobSubmit} ) => {
               className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              Add Job
+              Update Job
             </button>
           </div>
         </form>
